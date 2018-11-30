6560fe01c09b43abb2fcc7e7bcd5ea932c472c1d
pantheonsh <pantheonshell@yahoo.com>
2018-11-30 19:49:43 -0200

    Config pronta

--- config/config.go
+++ config/config.go
@@ -1,15 +1,32 @@
 package config
 
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+)
+
+// Config Configuração do bot
 type Config struct {
 	Token  string
 	Prefix string
 	Owner  string
 }
 
+// Data A configuração carregada
 var (
 	Data *Config
 )
 
 func Load() {
+	// First, we load the config file to memory
+	b, err := ioutil.ReadFile("config/config.json")
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
 
+	err = json.Unmarshal(b, &Data)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
 }
