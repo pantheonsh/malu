af7e4cacf00607cacb8733f148f4515dabf77485
pantheonsh <pantheonshell@yahoo.com>
2018-12-01 10:38:33 -0200

    Melhorar ping

--- commands/cmds/ping.go
+++ commands/cmds/ping.go
@@ -1,10 +1,29 @@
 package cmds
 
 import (
+	"strconv"
+	"time"
+
 	"github.com/bwmarrin/discordgo"
 )
 
 func PingCommand(args []string, s *discordgo.Session, m *discordgo.MessageCreate, c *discordgo.Channel) error {
-	s.ChannelMessageSend(c.ID, "Pong!")
+	msg, err := s.ChannelMessageSend(c.ID, "Aguarde.")
+
+	if err != nil {
+		return err
+	}
+
+	msgtimestamp, err := msg.Timestamp.Parse()
+
+	if err != nil {
+		return err
+	}
+
+	var t1 = msgtimestamp.Unix()
+	var t2 = time.Now().Unix()
+
+	s.ChannelMessageEdit(msg.ChannelID, msg.ID, "Ping: "+strconv.FormatInt((t2-t1)*2, 10)+"ms")
+
 	return nil
 }
